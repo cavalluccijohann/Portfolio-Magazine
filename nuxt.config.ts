dde1a683ee4eb52ab089c30381fed39d0a4cb822
johann <johann.cavallucci@epitech.eu>
2024-10-03 13:41:26 +0200

    🔥[ FEAT ] : Added the Plausible

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 export default defineNuxtConfig({
+
+  head: {
+    script: [
+      {
+        src: "https://analytics.hrcd.fr/js/script.js",
+        defer: true,
+        "data-domain": "foliomagazine.johanncvl.com",
+      },
+    ],
+  },
   css: ["~/assets/style/main.scss"],
 
   runtimeConfig: {
