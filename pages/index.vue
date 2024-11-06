84513423c16173d4bea16737d6ab9308cf06f7b5
Johann Cavallucci <72015679+cavalluccijohann@users.noreply.github.com>
2024-11-06 10:08:44 +0100

    Added the seo, description, title and keywords

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,24 @@
 <script lang="ts" setup>
 import {Toaster} from "vue-sonner";
 
+  useHead({
+  title: "Modern Portfolio Magazine Template",
+  titleTemplate: "",
+  meta: [
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "keywords", content: "portfolio template, magazine portfolio, creative portfolio template, Nuxt portfolio, Tailwind CSS portfolio, TypeScript portfolio, Resend email integration, personal branding template, web developer portfolio" },
+    { name: "description", content: "Discover a unique, magazine-inspired portfolio template for designers and professionals. Built with Nuxt, Tailwind CSS, and TypeScript, it’s fully customizable and optimized for impactful personal branding." },
+    { name: "author", content: "Johann Cavallucci" },
+  ],
+  link: [
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico",
+    },
+  ],
+});
+
 definePageMeta({
   title: "Home",
   name: "Home",
